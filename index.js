98cd5ff8a51c06a59c1a32731fcd0434a936483c
williamsbitbase <kennywill2027@gmail.com>
2025-03-10 23:22:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3002;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(
+  express.static(path.join(__dirname, "dist"), {
+    setHeaders: (res, filePath) => {
+      if (filePath.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      }
+    },
+  })
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port : ${PORT}`);
+});
