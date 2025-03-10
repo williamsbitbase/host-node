378275070189579fa9485472445750efcb338534
williamsbitbase <kennywill2027@gmail.com>
2025-03-11 00:15:02 +0100

    changed from dist, index.html to public, dist, index.html

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(
-  express.static(path.join(__dirname,"public", "dist"), {
+  express.static(path.join(__dirname,"public"), {
     setHeaders: (res, filePath) => {
       if (filePath.endsWith(".js")) {
         res.setHeader("Content-Type", "application/javascript");
